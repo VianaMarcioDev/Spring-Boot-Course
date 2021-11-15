7d3796dcecc75796b0677a46c3df126f53241902
Márcio de Freitas Viana <marcioviana.developer@gmail.com>
2021-11-15 15:16:49 -0300

    Migrar pacote Model/Util

---
+++ src/main/java/br/com/zup/Spring/Boot/Essentials/Course/util/DateUtil.java
@@ -0,0 +1,15 @@
+package br.com.zup.Spring.Boot.Essentials.Course.util;
+
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Component
+public class DateUtil {
+
+    public String formatLocalDateTimeToDatabaseStyle(LocalDateTime localDateTime){
+        return DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss").format(localDateTime);
+
+    }
+}
