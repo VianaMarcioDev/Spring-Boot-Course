564f2b11e309762d8c335b12e76e7b6876c21e8d
Márcio de Freitas Viana <marcioviana.developer@gmail.com>
2021-11-15 15:15:55 -0300

    Migrar pacote EndPoint

---
+++ src/main/java/br/com/zup/Spring/Boot/Essentials/Course/endpoint/StudentEndPoint.java
@@ -0,0 +1,27 @@
+package br.com.zup.Spring.Boot.Essentials.Course.endpoint;
+
+import br.com.zup.awesome.model.Student;
+import br.com.zup.awesome.util.DateUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static java.util.Arrays.asList;
+
+@RestController
+@RequestMapping("student")
+public class StudentEndPoint {
+
+    @Autowired
+    DateUtil dateUtil;
+
+    @GetMapping("/list")
+    public List<Student>listAll(){
+        System.out.println(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
+        return asList(new Student("Márcio"), new Student("Aldilene"));
+    }
+}
