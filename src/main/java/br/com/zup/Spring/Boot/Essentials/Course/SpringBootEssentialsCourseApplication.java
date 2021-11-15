7f71a5819c744e2beae8993ccac2e5eb24a83dca
Márcio de Freitas Viana <marcioviana.developer@gmail.com>
2021-11-15 15:11:42 -0300

    Primeiro commit. Criar projeto

---
+++ src/main/java/br/com/zup/Spring/Boot/Essentials/Course/SpringBootEssentialsCourseApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.Spring.Boot.Essentials.Course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootEssentialsCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootEssentialsCourseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/zup/Spring/Boot/Essentials/Course/SpringBootEssentialsCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.Spring.Boot.Essentials.Course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootEssentialsCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
