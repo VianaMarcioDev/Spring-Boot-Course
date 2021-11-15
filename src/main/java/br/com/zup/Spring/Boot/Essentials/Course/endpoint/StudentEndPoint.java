e0ab2d5e4d1e614b1ee3b3b3d853675a33dec435
Márcio de Freitas Viana <marcioviana.developer@gmail.com>
2021-11-15 15:43:13 -0300

    Alterar método listAll

--- src/main/java/br/com/zup/Spring/Boot/Essentials/Course/endpoint/StudentEndPoint.java
+++ src/main/java/br/com/zup/Spring/Boot/Essentials/Course/endpoint/StudentEndPoint.java
@@ -4,6 +4,8 @@ package br.com.zup.Spring.Boot.Essentials.Course.endpoint;
 import br.com.zup.Spring.Boot.Essentials.Course.model.Student;
 import br.com.zup.Spring.Boot.Essentials.Course.util.DateUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,8 +23,8 @@ public class StudentEndPoint {
      DateUtil dateUtil;
 
     @GetMapping("/list")
-    public List<Student>listAll(){
+    public ResponseEntity<?>listAll(){
         System.out.println(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
-        return asList(new Student("Márcio"), new Student("Aldilene"));
+        return new ResponseEntity<>(Student.studentList, HttpStatus.OK);
     }
 }
